ecc4aa8d30d0c3eaa3a38dd01efedf36a5c12fd9
Sashanikitenkova <81653543+Sashanikitenkova@users.noreply.github.com>
2022-02-03 18:56:05 +0200

    2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-console.log(galleryItems);
+const container = document.querySelector(".gallery");
+const cardGallery = createGallery(galleryItems);
+
+container.insertAdjacentHTML('beforeend', cardGallery);
+
+function createGallery (images) {
+    return images.map(({ preview, original, description }) => {
+        return `
+          <a class="gallery__item" href="${original}">
+            <img
+               class="gallery__image"
+               src="${preview}"
+               data-source="${original}"
+               alt="${description}"
+               
+            />
+           </a>
+         `;
+    })
+    .join('');  
+}
+
+    const gallery = new SimpleLightbox('.gallery a', {captionsData: 'alt'});
+
+    // container.addEventListener("click", gallery.next());
+
+
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import Player from '@vimeo/player';
 
+    const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+    
+    // player.on('play', function() {
+    //     console.log('played the video!');
+    // });
+
+    player.on('timeupdate', function(data) {
+        // data is an object containing properties specific to that event
+        // localStorage.setItem('videoplayer-current-time', JSON.stringify({data}));
+        const scienceTime = data.seconds;
+        // console.log(scienceTime);
+        localStorage.setItem('videoplayer-current-time', JSON.stringify({scienceTime}));
+    });
+
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
+
+
+  
+
+   
