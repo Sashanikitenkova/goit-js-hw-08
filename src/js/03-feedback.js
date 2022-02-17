d63de6c2ce648186a5cb5b6ecefa1af260119611
Sashanikitenkova <dolotova.a2017@gmail.com>
2022-02-17 23:21:29 +0200

    1

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -13,27 +13,38 @@ const refs = {
 };
 
 refs.form.addEventListener('submit', onFormSubmit);
-refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
+refs.form.addEventListener('input', throttle(onTextareaInput, 500));
 
-refs.form.addEventListener('input', e => {
-            // console.log(e.target.name);
-            // console.log(e.target.value);
+// refs.form.addEventListener('input', e => {
+//             // console.log(e.target.name);
+//             // console.log(e.target.value);
         
-            formData[e.target.name] = e.target.value;
-            console.log(formData);
-        });
+//             formData[e.target.name] = e.target.value;
+//             console.log(formData);
+//         });
     
 
 populateTextarea();
 
 function onFormSubmit(evt) {
-    evt.target.name.preventDefault();
+    evt.preventDefault();
+
+    const formEl = evt.currentTarget.elements;
+    const email = formEl.email.value;
+    const message = formEl.message.value;
+    if (!email || !message) {
+       alert("Все поля должны быть заполненны");
+       return;
+    };
+
     evt.currentTarget.reset();
     localStorage.removeItem(STORAGE_KEY);
 };
 
-function onTextareaInput() {
-    // console.log(massage);
+function onTextareaInput(e) {
+
+    formData[e.target.name] = e.target.value;
+    console.log(formData);
 
     localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 };
@@ -52,53 +63,64 @@ function populateTextarea() {
 
 
 
-// const massage = JSON.stringify(formData);
 
 
-
-// Записать в локал сторидж 
-// localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-
-// Забрать с локал сторидж
-//  const saveData = localStorage.getItem(STORAGE_KEY);
-//  const parsedData = JSON.parse(saveData);
-
+// refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
 
 // refs.form.addEventListener('input', e => {
-//         // console.log(e.target.name);
-//         // console.log(e.target.value);
+//             // console.log(e.target.name);
+//             // console.log(e.target.value);
+        
+//             formData[e.target.name] = e.target.value;
+//             console.log(formData);
+//         });
     
-//         formData[e.target.name] = e.target.value;
-//         console.log(formData);
-//     });
 
+// populateTextarea();
 
+// function onFormSubmit(e) {
+//     e.target.email.preventDefault();
+//     e.target.message.preventDefault();
 
-
-
-
-
-
-
-
-// function onFormSubmit(evt) {
-//     evt.preventDefault();
-//     evt.currentTarget.reset();
+//     if (!e.target.email || !e.target.message) {
+//         alert("Все поля должны быть заполненны");
+//         return;
+//      }
+     
+//     e.currentTarget.email.reset();
+//     e.currentTarget.message.reset();
 //     localStorage.removeItem(STORAGE_KEY);
 // };
 
-// function onTextareaInput(evt) {
-//     const massage = evt.target.value;
+// function onTextareaInput() {
+
 //     // console.log(massage);
 
-//     localStorage.setItem(STORAGE_KEY, massage);
+//     localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 // };
 
 // function populateTextarea() {
 //     const savedMessage = localStorage.getItem(STORAGE_KEY);
+//     const parsedData = JSON.parse(savedMessage);
 
-//     if(savedMessage) {
-//         console.log(savedMessage); 
-//         refs.textarea.value = savedMessage;
+//     if(parsedData) {
+//         console.log(parsedData); 
+//         refs.textarea.value = parsedData.message;
+//         refs.input.value = parsedData.email;
 //     };
 // };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
